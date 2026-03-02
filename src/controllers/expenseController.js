8d76b864d902995106339027a9ce987773811b3d
sarahdidulo <didulo.sarah@gmail.com>
2026-03-02 10:24:54 +0800

    trial with year and month filter (not yet implemented)

--- src/controllers/expenseController.js
+++ src/controllers/expenseController.js
@@ -3,7 +3,10 @@ import User from "../models/User.js";
 
 export const addExpense = async(req, res, next) => {
     try {
+        
         const newExpense = await Expense.create(req.body);
+        newExpense.dateOfTransaction = new Date(req.body.dateOfTransaction);
+        await newExpense.save();
         const user_transactor = await User.findOne({ _id: req.body.transactor_id}).populate('transactions');
         console.log("user transactor", user_transactor);
         console.log("new expense", newExpense);
@@ -19,6 +22,53 @@ export const getAllTransactions = async(req, res, next) => {
     try {
         const userTransactions = await User.findOne({_id: req.params.id}).populate('transactions').exec();
         res.send(userTransactions.transactions);
+    } catch (err) {
+        res.status(400).send(`ERROR: ${err}`);
+    }
+}
+
+function convertTransactionDate (transaction_date) {
+    const converted_transaction_date = new Date(transaction_date);
+    let month = converted_transaction_date.getMonth() + 1;
+    let day = converted_transaction_date.getDate();
+    if(month.toString().length === 1) {
+        month = `0` + month.toString();
+    }
+    if(day.toString().length === 1) {
+        day = `0` + day.toString();
+    }
+    return `${converted_transaction_date.getFullYear()}` + `-` + `${month}` + `-${day}`;
+}
+
+export const filterTransactions = async(req, res, next) => {
+    try {
+        const month = req.params.month;
+        const year = req.params.year;
+        const user_id = req.params.id;
+        const startDate = convertTransactionDate(new Date(`${year}-${month}-01`));
+        const endDate = convertTransactionDate(new Date(`${year}-${Number(month) + 1}-01`));
+        // if(month === '' && year == '') {
+        //     console.log('no month and year yet')
+        //     res.send('')
+        // } 
+        console.log("start date", startDate);
+        console.log("end date", endDate);
+
+        // let userTransactions = await Expense.where('transactor_id', user_id)
+        
+        const execExample = async () => {
+        const result = await Expense.where('transactor_id', user_id)
+            .aggregate([{
+                $project: { name: 1, category: 0 }
+            }])
+            .exec();
+        console.log(result);
+        }
+        execExample();
+    
+        // res.send(userTransactions);
+    
+        
     } catch (err) {
         res.status(400).send(`ERROR: ${err}`);
     }
--- src/routes/expenseRoutes.js
+++ src/routes/expenseRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { addExpense, getAllTransactions } from "./../controllers/expenseController.js";
+import { addExpense, getAllTransactions, filterTransactions } from "./../controllers/expenseController.js";
 
 const router = express.Router();
 
 router.post('/add-expense/', addExpense);
 router.get('/all/:id', getAllTransactions);
+router.get('/all/:id/:month/:year', filterTransactions);
 
 export default router;
