f612b8a6997f3d9c5704ff0108587e8182905854
sarahdidulo <didulo.sarah@gmail.com>
2026-03-06 10:19:47 +0800

    fixes

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
-// import 'dotenv/config';
-// import cookieParser from "cookie-parser";
+import 'dotenv/config';
+import cookieParser from "cookie-parser";
 
 import mainRouter from "./src/routes/index.js";
 
@@ -36,7 +36,7 @@ try {
 // origin: "http://127.0.0.1:5173"
 // Use express.json() middleware to parse JSON request bodies
 app.use(express.json());
-// app.use(cookieParser());
+app.use(cookieParser());
 // app.use(cors({ credentials: true , origin: "http://localhost:5173"}))
 // app.use(cors({ credentials: true , origin: "https://expense-tracker-app-phx9.onrender.com"}))
 
